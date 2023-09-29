87abd17bc0b073a98d66629b1da89c5c896c503b
egor_ <egor_spaik05@mail.ru>
2023-09-30 00:14:17 +0300

    Init commit

---
+++ Simplex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplex_Method
+{
+    public class LinearProgrammingProblem
+    {
+        public class Constraint
+        {
+            public List<double> Coefficients { get; set; }
+            public double RightHandSide { get; set; }
+        }
+
+        public List<double> ObjectiveFunctionCoefficients { get; set; }
+        public List<Constraint> Constraints { get; set; }
+
+        public LinearProgrammingProblem()
+        {
+            ObjectiveFunctionCoefficients = new List<double>();
+            Constraints = new List<Constraint>();
+        }
+    }
+
+}
